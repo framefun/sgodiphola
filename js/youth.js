61cf2f11eb1719f576f319a9713bfe9ed81382e9
clinsm <muleyaclins@live.com>
2020-07-01 00:24:19 +0200

    Update youth.js

--- js/youth.js
+++ js/youth.js
@@ -1,24 +1,27 @@
-const navSlide = () =>{
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
-
-  burger.addEventListener('click', () =>{
-    //toggle nav
-    nav.classList.toggle('nav-active');
-
-    //Animate links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = '';
-      }else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3 }s`;
-      }
-    });
-    //burger animation
+//the menu
+  const burger = document.getElementById('burger');
+  const fill_menu = document.getElementById('menu');
+  var menu = document.querySelector(".menu");
+  var hidden_menu = document.querySelector(".burgerMenu");
+  //fill_menu.style.borderRadius = "50%";
+  const nav_wrapper = document.getElementById('nav_wrapper');
+  burger.addEventListener('click',function(){
     burger.classList.toggle('toggle');
+    nav_wrapper.classList.toggle('open-menu');
+    fill_menu.classList.toggle('menu');
+    menu.style.animationName = "unfill";
+
   });
+//scroll to hide menu
+if (window.innerWidth > 1024) {
+  window.addEventListener("scroll",function(){
+    if(window.pageYOffset > 500){
+     hidden_menu.style.display = "block";
+    }
+    else if(window.pageYOffset < 500){
+      hidden_menu.style.display = "none";
+    }
 
-
+  },false);
 }
-navSlide();
+//the end of menu
